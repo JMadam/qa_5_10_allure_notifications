d587ebc7ec573685c092dee0f20ec9146fa30e6e
yuliyamadaminova <Cdtnjxrf2103>
2021-05-06 15:48:30 +0300

    Added tests

---
+++ src/test/java/PositiveTests.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PositiveTests {
+    @Test
+    void some1Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    void some2Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    void some3Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    void some4Test() {
+        assertTrue(true);
+    }
+}
